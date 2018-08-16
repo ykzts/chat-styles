9589d24458ac219a71d2e40f68be87e33883d268
Yamagishi Kazutoshi <ykzts@desire.sh>
2018-08-17 02:15:26 +0900

    Introduce @babel/plugin-transform-react-inline-elements

--- .babelrc.js
+++ .babelrc.js
@@ -2,12 +2,15 @@ const env = process.env.BABEL_ENV || process.env.NODE_ENV || 'development';
 
 module.exports = {
   plugins: [
-    ...(env === 'test' ? [
-      '@babel/transform-modules-commonjs',
-    ] : []),
     '@babel/proposal-class-properties',
     '@babel/proposal-object-rest-spread',
     ['@babel/proposal-pipeline-operator', { proposal: 'minimal' }],
+    ...(env === 'test' ? [
+      '@babel/transform-modules-commonjs',
+    ] : []),
+    ...(env === 'production' ? [
+      '@babel/transform-react-inline-elements',
+    ] : []),
   ],
   presets: [
     '@babel/flow',
