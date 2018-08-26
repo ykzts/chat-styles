1c24bb48b01f92d7b6c55fb5961ada2ab112fa93
Yamagishi Kazutoshi <ykzts@desire.sh>
2018-08-27 01:27:25 +0900

    For Apple

--- src/components/Html.jsx
+++ src/components/Html.jsx
@@ -21,8 +21,10 @@ export default ({ scripts, title }: Props) => (
       </title>
       <link href="/manifest.json" rel="manifest" type="application/manifest+json" />
       <link href="/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+      <link href="/favicon-192x192.png" rel="apple-touch-icon" type="image/png" />
       <link href={logo} rel="icon" type="image/png" />
       <meta content="#ffc107" name="theme-color" />
+      <meta content="yes" name="apple-mobile-web-app-capable" />
     </head>
     <body>
       <svg style={{ display: 'none' }}>
