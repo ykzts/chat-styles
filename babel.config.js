06d1f945a87c726cd14bbb4324b9de864c6d5ef3
Yamagishi Kazutoshi <ykzts@desire.sh>
2018-08-28 17:10:50 +0900

    func

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,23 @@
-const env = process.env.BABEL_ENV || process.env.NODE_ENV || 'development';
+module.exports = (api) => {
+  const env = api.env();
 
-module.exports = {
-  plugins: [
-    '@babel/proposal-class-properties',
-    '@babel/proposal-object-rest-spread',
-    ['@babel/proposal-pipeline-operator', { proposal: 'minimal' }],
-    env === 'test' && '@babel/transform-modules-commonjs',
-    env === 'production' && '@babel/transform-react-inline-elements',
-  ].filter(Boolean),
-  presets: [
-    '@babel/flow',
-    [
-      '@babel/react',
-      {
-        development: env !== 'production',
-        useBuiltIns: true,
-      },
+  return {
+    plugins: [
+      '@babel/proposal-class-properties',
+      '@babel/proposal-object-rest-spread',
+      ['@babel/proposal-pipeline-operator', { proposal: 'minimal' }],
+      env === 'test' && '@babel/transform-modules-commonjs',
+      env === 'production' && '@babel/transform-react-inline-elements',
+    ].filter(Boolean),
+    presets: [
+      '@babel/flow',
+      [
+        '@babel/react',
+        {
+          development: env !== 'production',
+          useBuiltIns: true,
+        },
+      ],
     ],
-  ],
+  };
 };
