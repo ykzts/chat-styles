8d9e0e8938799e9cd580280534d9f4dbbc6505d7
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-04-19 03:59:54 +0900

    fix: use react-helmet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,40 @@
 import { AppProps } from 'next/app'
 import React, { FC } from 'react'
+import { Helmet } from 'react-helmet'
 import Layout from 'components/templates/Layout'
 
 import 'prismjs/themes/prism-okaidia.css'
 
+const description =
+  'Chat StylesはYouTube Liveのチャットの見た目を変更するCSSを生成するためのツールです。OBS StudioのブラウザソースのカスタムCSSとして使われることを前提としています。'
+
 const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <Layout title="Chat Styles">
-    <Component {...pageProps} />
-  </Layout>
+  <>
+    <Helmet
+      defaultTitle="Chat Styles"
+      htmlAttributes={{ lang: 'ja' }}
+      titleTemplate="%s - Chat Styles"
+    >
+      <meta charSet="UTF-8" />
+      <meta content="width=device-width" name="viewport" />
+      <meta content={description} name="description" />
+      <meta content="/favicon-192x192.png" property="og:image" />
+      <meta content="192" property="og:image:height" />
+      <meta content="192" property="og:image:width" />
+      <meta content="Chat Styles" property="og:title" />
+      <meta content="website" property="og:type" />
+      <meta content="summary" name="twitter:card" />
+      <meta content={description} name="twitter:description" />
+      <meta content="/favicon-192x192.png" name="twitter:image" />
+      <meta content="Chat Styles" name="twitter:title" />
+      <link href="/manifest.json" rel="manifest" />
+      <link href="/favicon.ico" rel="icon" />
+    </Helmet>
+
+    <Layout title="Chat Styles">
+      <Component {...pageProps} />
+    </Layout>
+  </>
 )
 
 export default App
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,34 +1,19 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 export default class MyDocument extends Document {
   render(): JSX.Element {
+    const helmet = Helmet.renderStatic()
+
     return (
-      <Html lang="ja">
+      <Html {...helmet.htmlAttributes.toComponent()}>
         <Head>
-          <meta charSet="UTF-8" />
-          <meta content="width=device-width" name="viewport" />
-          <meta content="#ffc107" name="theme-color" />
-          <link href="/manifest.json" rel="manifest" />
-          <link href="/favicon.ico" rel="icon" />
-          <meta
-            content="Chat StylesはYouTube Liveのチャットの見た目を変更するCSSを生成するためのツールです。OBS StudioのブラウザソースのカスタムCSSとして使われることを前提としています。"
-            name="description"
-          />
-          <meta content="/favicon-192x192.png" property="og:image" />
-          <meta content="192" property="og:image:height" />
-          <meta content="192" property="og:image:width" />
-          <meta content="" property="og:title" />
-          <meta content="website" property="og:type" />
-          <meta content="summary" name="twitter:card" />
-          <meta
-            content="Chat StylesはYouTube Liveのチャットの見た目を変更するCSSを生成するためのツールです。OBS StudioのブラウザソースのカスタムCSSとして使われることを前提としています。"
-            name="twitter:description"
-          />
-          <meta content="/favicon-192x192.png" name="twitter:image" />
-          <meta content="Chat Styles" name="twitter:title" />
+          {helmet.title.toComponent()}
+          {helmet.meta.toComponent()}
+          {helmet.link.toComponent()}
         </Head>
-        <body>
+        <body {...helmet.bodyAttributes.toComponent()}>
           <Main />
           <NextScript />
         </body>
