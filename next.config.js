8014db68b7063cfea25c591f0a196d231427299c
Yamagishi Kazutoshi <ykzts@desire.sh>
2021-04-23 07:47:26 +0900

    refactor: extract headers and rewriters

--- next.config.js
+++ next.config.js
@@ -1,62 +1,13 @@
 const withPWA = require('next-pwa')
+const headers = require('./headers.json')
+const rewrites = require('./rewrites.json')
 
 const nextConfig = {
   future: {
     webpack5: true
   },
   async headers() {
-    return [
-      {
-        headers: [
-          {
-            key: 'cache-control',
-            value: 'max-age=600, s-maxage=60'
-          },
-          {
-            key: 'content-security-policy',
-            value:
-              "connect-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com https://polyfill.io; default-src 'none'; font-src https://fonts.gstatic.com; frame-src 'self'; img-src 'self' data:; manifest-src 'self'; sandbox allow-popups allow-same-origin allow-scripts; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://storage.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; worker-src 'self'"
-          },
-          {
-            key: 'referrer-policy',
-            value: 'same-origin, strict-origin-when-cross-origin'
-          }
-        ],
-        source: '/((?!_next/).*)'
-      },
-      {
-        headers: [
-          {
-            key: 'content-security-policy',
-            value:
-              "default-src 'none'; font-src https://fonts.gstatic.com; img-src 'self' data:; script-src 'unsafe-inline' https://polyfill.io; style-src 'unsafe-inline' blob: https://fonts.googleapis.com"
-          },
-          {
-            key: 'referrer-policy',
-            value: 'no-referrer'
-          }
-        ],
-        source: '/preview/(.*)'
-      },
-      {
-        headers: [
-          {
-            key: 'cache-control',
-            value: 'max-age=0'
-          }
-        ],
-        source: '/service-worker.js'
-      },
-      {
-        headers: [
-          {
-            key: 'content-type',
-            value: 'application/manifest+json'
-          }
-        ],
-        source: '/manifest.json'
-      }
-    ]
+    return headers
   },
   pwa: {
     dest: '.next/static',
@@ -64,24 +15,7 @@ const nextConfig = {
     sw: 'service-worker.js'
   },
   async rewrites() {
-    return [
-      {
-        destination: '/_next/static/service-worker.js',
-        source: '/service-worker.js'
-      },
-      {
-        destination: '/_next/static/service-worker.js.map',
-        source: '/service-worker.js.map'
-      },
-      {
-        destination: '/_next/static/workbox-:hash.js',
-        source: '/workbox-:hash.js'
-      },
-      {
-        destination: '/_next/static/workbox-:hash.js.map',
-        source: '/workbox-:hash.js.map'
-      }
-    ]
+    return rewrites
   }
 }
 
