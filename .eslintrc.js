ab18c0fe7d2e87c5745f6be2fced3e9e9862b55a
Yamagishi Kazutoshi <ykzts@desire.sh>
2021-12-04 23:56:13 +0900

    fix: ran prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['eslint:recommended', 'next/core-web-vitals', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended'
+  ],
   overrides: [
     {
       env: {
